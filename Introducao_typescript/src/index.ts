89e4a0e349df37ab4c7475ff90e0e67e013b77b5
luis <henrique3.terceiro@gmail.com>
2025-03-06 21:59:20 -0300

    Criando um array com numeros e somando eles

--- Introducao_typescript/src/index.ts
+++ Introducao_typescript/src/index.ts
@@ -1,2 +1,15 @@
 let mensagem: string = "Olá, TypeScript!";
 console.log(mensagem);
+
+//Exercicio 1
+
+// Criando o array com tipagem
+const numeros: number[] = [10, 20, 30, 40, 50];
+
+// Função para somar os valores do array
+const soma = (numeros: number[]): number => {
+    return numeros.reduce((acc, num) => acc + num, 0);
+}
+
+// Print
+console.log(`A soma dos valores é: ${soma(numeros)}`);
